00616639961518428688e9d36dc28151c625fca1
HC-kang <weston0713@gmail.com>
2021-07-09 23:56:23 +0900

    0708

--- ch07/4.noise.py
+++ ch07/4.noise.py
@@ -1,29 +1,30 @@
+import os 
+os.chdir('/Users/heechankang/projects/pythonworkspace/git_study/opencv/ch07')
 import numpy as np
 import cv2
 import random
-
 
 def noise_gaussian():
     src = cv2.imread('lenna.bmp', cv2.IMREAD_GRAYSCALE)
 
     if src is None:
-        print('Image load failed!')
+        print('사진 불러오기 실패')
         return
-
+    
     cv2.imshow('src', src)
 
     for stddev in [10, 20, 30]:
         noise = np.zeros(src.shape, np.int32)
         cv2.randn(noise, 0, stddev)
 
-        dst = cv2.add(src, noise, dtype=cv2.CV_8UC1)
+        dst = cv2.add(src, noise, dtype = cv2.CV_8UC1)
 
-        desc = 'stddev = %d' % stddev
+        desc = f'stddev = {stddev}'
         cv2.putText(dst, desc, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
-                   1.0, 255, 1, cv2.LINE_AA)
+                    1.0, 255, 1, cv2.LINE_AA)
         cv2.imshow('dst', dst)
         cv2.waitKey()
-
+    
     cv2.destroyAllWindows()
 
 
@@ -31,16 +32,16 @@ def filter_bilateral():
     src = cv2.imread('lenna.bmp', cv2.IMREAD_GRAYSCALE)
 
     if src is None:
-        print('Image load failed!')
+        print('사진 불러오기 실패')
         return
-
+    
     noise = np.zeros(src.shape, np.int32)
     cv2.randn(noise, 0, 5)
-    cv2.add(src, noise, src, dtype=cv2.CV_8UC1)
+    cv2.add(src, noise, src, dtype = cv2.CV_8UC1)
 
     dst1 = cv2.GaussianBlur(src, (0, 0), 5)
     dst2 = cv2.bilateralFilter(src, -1, 10, 5)
-    
+
     cv2.imshow('src', src)
     cv2.imshow('dst1', dst1)
     cv2.imshow('dst2', dst2)
@@ -52,16 +53,16 @@ def filter_median():
     src = cv2.imread('lenna.bmp', cv2.IMREAD_GRAYSCALE)
 
     if src is None:
-        print('Image load failed!')
+        print('이미지 불러오기 실패')
         return
 
     for i in range(0, int(src.size / 10)):
-        x = random.randint(0, src.shape[1] - 1)
-        y = random.randint(0, src.shape[0] - 1)
+        x = random.randint(0, src.shape[1] -1)
+        y = random.randint(0, src.shape[0] -1)
         src[x, y] = (i % 2) * 255
 
     dst1 = cv2.GaussianBlur(src, (0, 0), 1)
-    dst2 = cv2.medianBlur(src, 3)
+    dst2 = cv2.medianBlur(src,3)
 
     cv2.imshow('src', src)
     cv2.imshow('dst1', dst1)
@@ -69,8 +70,86 @@ def filter_median():
     cv2.waitKey()
     cv2.destroyAllWindows()
 
-
 if __name__ == '__main__':
     noise_gaussian()
     filter_bilateral()
     filter_median()
+
+#####
+#####
+# import numpy as np
+# import cv2
+# import random
+
+
+# def noise_gaussian():
+#     src = cv2.imread('lenna.bmp', cv2.IMREAD_GRAYSCALE)
+
+#     if src is None:
+#         print('Image load failed!')
+#         return
+
+#     cv2.imshow('src', src)
+
+#     for stddev in [10, 20, 30]:
+#         noise = np.zeros(src.shape, np.int32)
+#         cv2.randn(noise, 0, stddev)
+
+#         dst = cv2.add(src, noise, dtype=cv2.CV_8UC1)
+
+#         desc = 'stddev = %d' % stddev
+#         cv2.putText(dst, desc, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+#                    1.0, 255, 1, cv2.LINE_AA)
+#         cv2.imshow('dst', dst)
+#         cv2.waitKey()
+
+#     cv2.destroyAllWindows()
+
+
+# def filter_bilateral():
+#     src = cv2.imread('lenna.bmp', cv2.IMREAD_GRAYSCALE)
+
+#     if src is None:
+#         print('Image load failed!')
+#         return
+
+#     noise = np.zeros(src.shape, np.int32)
+#     cv2.randn(noise, 0, 5)
+#     cv2.add(src, noise, src, dtype=cv2.CV_8UC1)
+
+#     dst1 = cv2.GaussianBlur(src, (0, 0), 5)
+#     dst2 = cv2.bilateralFilter(src, -1, 10, 5)
+    
+#     cv2.imshow('src', src)
+#     cv2.imshow('dst1', dst1)
+#     cv2.imshow('dst2', dst2)
+#     cv2.waitKey()
+#     cv2.destroyAllWindows()
+
+
+# def filter_median():
+#     src = cv2.imread('lenna.bmp', cv2.IMREAD_GRAYSCALE)
+
+#     if src is None:
+#         print('Image load failed!')
+#         return
+
+#     for i in range(0, int(src.size / 10)):
+#         x = random.randint(0, src.shape[1] - 1)
+#         y = random.randint(0, src.shape[0] - 1)
+#         src[x, y] = (i % 2) * 255
+
+#     dst1 = cv2.GaussianBlur(src, (0, 0), 1)
+#     dst2 = cv2.medianBlur(src, 3)
+
+#     cv2.imshow('src', src)
+#     cv2.imshow('dst1', dst1)
+#     cv2.imshow('dst2', dst2)
+#     cv2.waitKey()
+#     cv2.destroyAllWindows()
+
+
+# if __name__ == '__main__':
+#     noise_gaussian()
+#     filter_bilateral()
+#     filter_median()
--- ch09/2.hough.py
+++ ch09/2.hough.py
@@ -4,21 +4,21 @@ import numpy as np
 import cv2
 import math
 
-
 def hough_lines():
     src = cv2.imread('building.jpg', cv2.IMREAD_GRAYSCALE)
 
     if src is None:
-        print('Image load failed!')
+        print('사진 불러오기에 실패')
         return
 
     edge = cv2.Canny(src, 50, 150)
-    lines = cv2.HoughLines(edge, 1, math.pi / 180, 250)  # rho=1, theta=1도, 축열배열의 임계값=250 이상이면 직선으로 판단
+    lines = cv2.HoughLines(edge, 1, math.pi / 180, 250)
+    # rho=1, theta=1도, 축열배열의 임계값=250 이상이면 직선으로 판단
 
     dst = cv2.cvtColor(edge, cv2.COLOR_GRAY2BGR)
 
     if lines is not None:
-        for i in range(lines.shape[0]):          # 모든 직선을 두께가 2인 빨간색으로 표시
+        for i in range(lines.shape[0]):
             rho = lines[i][0][0]
             theta = lines[i][0][1]
             cos_t = math.cos(theta)
@@ -39,11 +39,13 @@ def hough_line_segments():
     src = cv2.imread('building.jpg', cv2.IMREAD_GRAYSCALE)
 
     if src is None:
-        print('Image load failed!')
+        print('이미지 불러오기 실패')
         return
 
     edge = cv2.Canny(src, 50, 150)
-    lines = cv2.HoughLinesP(edge, 1, math.pi / 180, 160, minLineLength=50, maxLineGap=5)  # 확률적 허프변환, 검출할 최소길이는 50, 직선으로 간주할 최대 에지 점 간격은 5
+    lines = cv2.HoughLinesP(edge, 1, math.pi / 180, 160, minLineLength = 50,
+                            maxLineGap=5)
+                            # 확률적 허프변환, 최소길이 50, 최대 에지 점 간격은 5
 
     dst = cv2.cvtColor(edge, cv2.COLOR_GRAY2BGR)
 
@@ -59,16 +61,15 @@ def hough_line_segments():
     cv2.destroyAllWindows()
 
 
-def hough_circles():   # cv2.HoughCircles() 내부에서는 Sobel()과 Canny()로 그래디언트와 에지 계산. 
+def hough_circles(): # HoughCircle에서는 sobel과 canny 활용해서 gradient 계산
     src = cv2.imread('coins.png', cv2.IMREAD_GRAYSCALE)
 
     if src is None:
-        print('Image load failed!')
+        print('사진 불러오기 실패')
         return
-
-    blurred = cv2.blur(src, (3, 3))
-    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, 50,    # 입력 영상과 같은크기(1)의 축열배열, 중심 최소 거리 50 이상일 때만 다른 원으로 인정
-                              param1=150, param2=30)
+    
+    blurred = cv2.blur(src, (3,3))
+    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, 50,param1 = 150, param2=30)
 
     dst = cv2.cvtColor(src, cv2.COLOR_GRAY2BGR)
 
@@ -76,14 +77,104 @@ def hough_circles():   # cv2.HoughCircles() 내부에서는 Sobel()과 Canny()�
         for i in range(circles.shape[1]):
             cx, cy, radius = circles[0][i]
             cv2.circle(dst, (cx, cy), int(radius), (0, 0, 255), 2, cv2.LINE_AA)
-
+    
     cv2.imshow('src', src)
     cv2.imshow('dst', dst)
     cv2.waitKey()
     cv2.destroyAllWindows()
 
-
 if __name__ == '__main__':
     hough_lines()
     hough_line_segments()
     hough_circles()
+
+
+#####
+#####
+# import numpy as np
+# import cv2
+# import math
+
+
+# def hough_lines():
+#     src = cv2.imread('building.jpg', cv2.IMREAD_GRAYSCALE)
+
+#     if src is None:
+#         print('Image load failed!')
+#         return
+
+#     edge = cv2.Canny(src, 50, 150)
+#     lines = cv2.HoughLines(edge, 1, math.pi / 180, 250)  # rho=1, theta=1도, 축열배열의 임계값=250 이상이면 직선으로 판단
+
+#     dst = cv2.cvtColor(edge, cv2.COLOR_GRAY2BGR)
+
+#     if lines is not None:
+#         for i in range(lines.shape[0]):          # 모든 직선을 두께가 2인 빨간색으로 표시
+#             rho = lines[i][0][0]
+#             theta = lines[i][0][1]
+#             cos_t = math.cos(theta)
+#             sin_t = math.sin(theta)
+#             x0, y0 = rho * cos_t, rho * sin_t
+#             alpha = 1000
+#             pt1 = (int(x0 - alpha * sin_t), int(y0 + alpha * cos_t))
+#             pt2 = (int(x0 + alpha * sin_t), int(y0 - alpha * cos_t))
+#             cv2.line(dst, pt1, pt2, (0, 0, 255), 2, cv2.LINE_AA)
+
+#     cv2.imshow('src', src)
+#     cv2.imshow('dst', dst)
+#     cv2.waitKey()
+#     cv2.destroyAllWindows()
+
+
+# def hough_line_segments():
+#     src = cv2.imread('building.jpg', cv2.IMREAD_GRAYSCALE)
+
+#     if src is None:
+#         print('Image load failed!')
+#         return
+
+#     edge = cv2.Canny(src, 50, 150)
+#     lines = cv2.HoughLinesP(edge, 1, math.pi / 180, 160, minLineLength=50, maxLineGap=5)  # 확률적 허프변환, 검출할 최소길이는 50, 직선으로 간주할 최대 에지 점 간격은 5
+
+#     dst = cv2.cvtColor(edge, cv2.COLOR_GRAY2BGR)
+
+#     if lines is not None:
+#         for i in range(lines.shape[0]):
+#             pt1 = (lines[i][0][0], lines[i][0][1])
+#             pt2 = (lines[i][0][2], lines[i][0][3])
+#             cv2.line(dst, pt1, pt2, (0, 0, 255), 2, cv2.LINE_AA)
+
+#     cv2.imshow('src', src)
+#     cv2.imshow('dst', dst)
+#     cv2.waitKey()
+#     cv2.destroyAllWindows()
+
+
+# def hough_circles():   # cv2.HoughCircles() 내부에서는 Sobel()과 Canny()로 그래디언트와 에지 계산. 
+#     src = cv2.imread('coins.png', cv2.IMREAD_GRAYSCALE)
+
+#     if src is None:
+#         print('Image load failed!')
+#         return
+
+#     blurred = cv2.blur(src, (3, 3))
+#     circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, 50,    # 입력 영상과 같은크기(1)의 축열배열, 중심 최소 거리 50 이상일 때만 다른 원으로 인정
+#                               param1=150, param2=30)
+
+#     dst = cv2.cvtColor(src, cv2.COLOR_GRAY2BGR)
+
+#     if circles is not None:
+#         for i in range(circles.shape[1]):
+#             cx, cy, radius = circles[0][i]
+#             cv2.circle(dst, (cx, cy), int(radius), (0, 0, 255), 2, cv2.LINE_AA)
+
+#     cv2.imshow('src', src)
+#     cv2.imshow('dst', dst)
+#     cv2.waitKey()
+#     cv2.destroyAllWindows()
+
+
+# if __name__ == '__main__':
+#     hough_lines()
+#     hough_line_segments()
+#     hough_circles()
