b7e867495f9bad512da08015d48eed77258e92e3
HC-kang <weston0713@gmail.com>
2021-07-13 13:56:47 +0900

    0713

--- ch11/2.adaptive.py
+++ ch11/2.adaptive.py
@@ -7,19 +7,17 @@ import cv2
 
 def on_trackbar(pos):
     bsize = pos
-    if bsize % 2 == 0: bsize = bsize - 1
+    if bsize % 2 == 0: bsize = bsize -1
     if bsize < 3: bsize = 3
 
     dst = cv2.adaptiveThreshold(src, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                               cv2.THRESH_BINARY, bsize, 5)
-
-    cv2.imshow('dst', dst)
-
+                                cv2.THRESH_BINARY, bsize, 5)
+    cv2.imread('dst', dst)
 
 src = cv2.imread('sudoku.jpg', cv2.IMREAD_GRAYSCALE)
 
 if src is None:
-    print('Image load failed!')
+    print('이미지 불러오기 실패')
     sys.exit()
 
 cv2.imshow('src', src)
@@ -30,3 +28,36 @@ cv2.setTrackbarPos('Block Size', 'dst', 11)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
+
+#####
+#####
+# import sys
+# import numpy as np
+# import cv2
+
+
+# def on_trackbar(pos):
+#     bsize = pos
+#     if bsize % 2 == 0: bsize = bsize - 1
+#     if bsize < 3: bsize = 3
+
+#     dst = cv2.adaptiveThreshold(src, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+#                                cv2.THRESH_BINARY, bsize, 5)
+
+#     cv2.imshow('dst', dst)
+
+
+# src = cv2.imread('sudoku.jpg', cv2.IMREAD_GRAYSCALE)
+
+# if src is None:
+#     print('Image load failed!')
+#     sys.exit()
+
+# cv2.imshow('src', src)
+
+# cv2.namedWindow('dst')
+# cv2.createTrackbar('Block Size', 'dst', 0, 200, on_trackbar)
+# cv2.setTrackbarPos('Block Size', 'dst', 11)
+
+# cv2.waitKey()
+# cv2.destroyAllWindows()
--- ch11/3.morphology.py
+++ ch11/3.morphology.py
@@ -2,33 +2,33 @@ import os
 os.chdir('/Users/heechankang/projects/pythonworkspace/git_study/opencv/ch11')
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 
 
 def erode_dilate():
     src = cv2.imread('milkdrop.bmp', cv2.IMREAD_GRAYSCALE)
 
     if src is None:
-        print('Image load failed!')
+        print('이미지 불러오기 실패')
         return
-
+    
     _, src_bin = cv2.threshold(src, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-    dst1 = cv2.erode(src_bin, None)          # src_bin에 대해 침식과 팽창 수행
+    dst1 = cv2.erode(src_bin, None)
     dst2 = cv2.dilate(src_bin, None)
 
     plt.subplot(221), plt.axis('off'), plt.imshow(src, 'gray'), plt.title('src')
-    plt.subplot(222), plt.axis('off'), plt.imshow(src_bin, 'gray'), plt.title('src_bin')    # 단순 이진화
-    plt.subplot(223), plt.axis('off'), plt.imshow(dst1, 'gray'), plt.title('erode')         # 침식
-    plt.subplot(224), plt.axis('off'), plt.imshow(dst2, 'gray'), plt.title('dilate')        # 팽창
+    plt.subplot(222), plt.axis('off'), plt.imshow(src_bin, 'gray'), plt.title('src_bin')
+    plt.subplot(223), plt.axis('off'), plt.imshow(dst1, 'gray'), plt.title('erode')
+    plt.subplot(224), plt.axis('off'), plt.imshow(dst2, 'gray'), plt.title('dilate')
     plt.show()
-
+    
 
 def open_close():
     src = cv2.imread('milkdrop.bmp', cv2.IMREAD_GRAYSCALE)
 
     if src is None:
-        print('Image load failed!')
+        print('이미지 불러오기 실패')
         return
 
     _, src_bin = cv2.threshold(src, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
@@ -38,11 +38,62 @@ def open_close():
 
     plt.subplot(221), plt.axis('off'), plt.imshow(src, 'gray'), plt.title('src')
     plt.subplot(222), plt.axis('off'), plt.imshow(src_bin, 'gray'), plt.title('src_bin')
-    plt.subplot(223), plt.axis('off'), plt.imshow(dst1, 'gray'), plt.title('open')        # 열기
-    plt.subplot(224), plt.axis('off'), plt.imshow(dst2, 'gray'), plt.title('close')       # 닫기
+    plt.subplot(223), plt.axis('off'), plt.imshow(dst1, 'gray'), plt.title('open')
+    plt.subplot(224), plt.axis('off'), plt.imshow(dst2, 'gray'), plt.title('close')
     plt.show()
 
 
 if __name__ == '__main__':
     erode_dilate()
     open_close()
+
+
+
+#####
+#####
+# import numpy as np
+# import cv2
+# from matplotlib import pyplot as plt
+
+
+# def erode_dilate():
+#     src = cv2.imread('milkdrop.bmp', cv2.IMREAD_GRAYSCALE)
+
+#     if src is None:
+#         print('Image load failed!')
+#         return
+
+#     _, src_bin = cv2.threshold(src, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+#     dst1 = cv2.erode(src_bin, None)          # src_bin에 대해 침식과 팽창 수행
+#     dst2 = cv2.dilate(src_bin, None)
+
+#     plt.subplot(221), plt.axis('off'), plt.imshow(src, 'gray'), plt.title('src')
+#     plt.subplot(222), plt.axis('off'), plt.imshow(src_bin, 'gray'), plt.title('src_bin')    # 단순 이진화
+#     plt.subplot(223), plt.axis('off'), plt.imshow(dst1, 'gray'), plt.title('erode')         # 침식
+#     plt.subplot(224), plt.axis('off'), plt.imshow(dst2, 'gray'), plt.title('dilate')        # 팽창
+#     plt.show()
+
+
+# def open_close():
+#     src = cv2.imread('milkdrop.bmp', cv2.IMREAD_GRAYSCALE)
+
+#     if src is None:
+#         print('Image load failed!')
+#         return
+
+#     _, src_bin = cv2.threshold(src, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+#     dst1 = cv2.morphologyEx(src_bin, cv2.MORPH_OPEN, None)
+#     dst2 = cv2.morphologyEx(src_bin, cv2.MORPH_CLOSE, None)
+
+
+#     plt.subplot(221), plt.axis('off'), plt.imshow(src, 'gray'), plt.title('src')
+#     plt.subplot(222), plt.axis('off'), plt.imshow(src_bin, 'gray'), plt.title('src_bin')
+#     plt.subplot(223), plt.axis('off'), plt.imshow(dst1, 'gray'), plt.title('open')        # 열기
+#     plt.subplot(224), plt.axis('off'), plt.imshow(dst2, 'gray'), plt.title('close')       # 닫기
+#     plt.show()
+
+# if __name__ == '__main__':
+#     erode_dilate()
+#     open_close()
